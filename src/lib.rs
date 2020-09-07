98fe3de7219df52e43a46b0759ce5a3e554391eb
Sachin Cherian <sachincs173@gmail.com>
2020-09-07 21:55:59 +0530

    initial application

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
-pub fn test() {
-    println!("Works!");
+pub trait VertexEngineApplication {
+    fn run(&mut self);
+}
+
+pub fn run(application: &mut impl VertexEngineApplication) {
+    application.run();
 }
